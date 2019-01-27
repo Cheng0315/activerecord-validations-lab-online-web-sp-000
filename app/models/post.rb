0c0d06ee8d3eac2b17977a1fdc4e59994b07bbff
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 13:01:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :click_baity_title
+
+  def click_baity_title
+    !["Won't Believe", "Secret", "Top 10", "Guess"].select {|ele| :name.include?(ele)}.empty?
+  end
 end
