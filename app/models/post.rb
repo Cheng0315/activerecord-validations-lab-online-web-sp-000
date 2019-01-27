8e9ecce70f1cf5bc4a2c017f4005f8bc5075c31a
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 13:13:49 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validate :name, inclusion: { in: %w(Won't Believe Secret Top 10 Guess)}
+  validates :name, inclusion: { in: %w(Won't Believe Secret Top 10 Guess)}
 
 end
